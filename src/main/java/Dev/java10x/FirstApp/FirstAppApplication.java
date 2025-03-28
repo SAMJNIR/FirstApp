b66524aab23cd1d723f6ea255f01078d876ad3ce
Sandro jr <testscodes312@gmail.com>
2025-03-27 21:47:21 -0300

    first commit

---
+++ src/main/java/Dev/java10x/FirstApp/FirstAppApplication.java
@@ -0,0 +1,13 @@
+package Dev.java10x.FirstApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAppApplication.class, args);
+	}
+
+}
